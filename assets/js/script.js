c437ee8c9580970b25f56afdb15c533dfd7c0181
GaryCooper-pm <gary@pixelmixel.co.uk>
2022-03-04 16:33:10 +0000

    Add constants to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 // Quiz JavaScript
 
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
 
 
 // End Quiz Javascript
