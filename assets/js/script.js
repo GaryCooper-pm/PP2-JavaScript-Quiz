36013bc5583b6646b7f2f31f73848322b735e517
GaryCooper-pm <gary@pixelmixel.co.uk>
2022-03-07 16:33:03 +0000

    Update link on line 172 of script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -169,7 +169,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
 
-        return window.location.assign('endquiz.html');
+        return window.location.assign('../endquiz.html');
     }
 
     questionCounter++;
