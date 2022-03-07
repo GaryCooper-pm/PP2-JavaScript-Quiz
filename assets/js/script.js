04a790e480c158751674974cda13dfc2fd428f38
GaryCooper-pm <gary@pixelmixel.co.uk>
2022-03-07 11:28:19 +0000

    Add javascript to save high score and username

--- assets/js/script.js
+++ assets/js/script.js
@@ -238,6 +238,13 @@ username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value
 })
 
-
+saveHighScore = e => {
+    e.preventDefault()
+    
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    }
+}
 
 // Highscores Javascript
