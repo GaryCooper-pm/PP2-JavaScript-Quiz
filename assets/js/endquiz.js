52b55289db393b77b941d1e2e2932749ae509f5e
GaryCooper-pm <gary@pixelmixel.co.uk>
2022-03-10 12:36:59 +0000

    Add jshint esversion 6 to js files

--- assets/js/endquiz.js
+++ assets/js/endquiz.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 // End Quiz Javascript
 
 const username = document.querySelector('#username');
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 // Quiz JavaScript
 
 const question = document.querySelector('#question');
